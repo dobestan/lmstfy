c8b1eb1a7d6adddb44c54b3d7755d674ee00a2a1
dobestan <dobestan@gmail.com>
2015-11-04 18:59:20 +0900

    update development settings : add django_extenstions, debug_toolbar

--- lmstfy/lmstfy/settings/development.py
+++ lmstfy/lmstfy/settings/development.py
@@ -1,1 +1,14 @@
 from .partials import *
+
+
+INSTALLED_APPS += (
+    'django_extensions',
+    'debug_toolbar',
+)
+
+
+# django-extensions
+# http://django-extensions.readthedocs.org
+
+# Always use IPython for shell_plus
+SHELL_PLUS = "ipython"
