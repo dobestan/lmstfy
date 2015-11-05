40a2aba02cf46e0de78e530487a5cabc3ce97adb
dobestan <dobestan@gmail.com>
2015-11-05 21:28:44 +0900

    integrate Sentry : for real-time crash reporting

--- lmstfy/lmstfy/settings/production.py
+++ lmstfy/lmstfy/settings/production.py
@@ -1,3 +1,7 @@
+import os
+
+import raven
+
 from .partials import *
 
 
@@ -6,3 +10,18 @@ DEBUG = False
 ALLOWED_HOSTS = [
     '*',
 ]
+
+
+INSTALLED_APPS += (
+    'raven.contrib.django.raven_compat',
+)
+
+
+# Sentry
+# https://getsentry.com/
+# https://www.getsentry.com/for/django/
+
+RAVEN_CONFIG = {
+    'dsn': os.environ.get('SENTRY_DSN'),
+    'release': raven.fetch_git_sha(PROJECT_ROOT),
+}
