5c3da3569de17c973ec48f3e346b8c9212b94135
dobestan <dobestan@gmail.com>
2015-11-06 16:31:29 +0900

    refactor search views : using SearchBaseView

---
+++ lmstfy/search/views/base.py
@@ -0,0 +1,11 @@
+from django.views.generic.detail import DetailView
+from django.shortcuts import redirect
+from django.contrib.sites.shortcuts import get_current_site
+
+
+class SearchBaseView(DetailView):
+    template_name = "base.html"
+    context_object_name = 'site'
+
+    def get_object(self):
+        return get_current_site(self.request)
--- lmstfy/search/views/result.py
+++ lmstfy/search/views/result.py
@@ -1,11 +1,5 @@
-from django.views.generic.detail import DetailView
-from django.shortcuts import redirect
-from django.contrib.sites.shortcuts import get_current_site
+from .base import SearchBaseView
 
 
-class SearchResultView(DetailView):
-    template_name = "base.html"
-    context_object_name = 'site'
-
-    def get_object(self):
-        return get_current_site(self.request)
+class SearchResultView(SearchBaseView):
+    pass
