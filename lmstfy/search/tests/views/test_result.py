414973f6f7b750832b8932a76ea0cab88904803e
dobestan <dobestan@gmail.com>
2015-11-08 16:21:00 +0900

    update SearchResultView : create history instance on request

--- lmstfy/search/tests/views/test_result.py
+++ lmstfy/search/tests/views/test_result.py
@@ -2,7 +2,7 @@ from django.core.urlresolvers import reverse
 
 from .base import SearchBaseTestCase
 from .mixins.test_request import SearchRequestTestCaseMixin
-from search.models import Query, Result
+from search.models import Query, Result, History
 
 
 class SearchResultViewTestCase(SearchBaseTestCase, SearchRequestTestCaseMixin):
@@ -38,3 +38,32 @@ class SearchResultViewTestCase(SearchBaseTestCase, SearchRequestTestCaseMixin):
             self.response,
             self.query.content,
         )
+
+    def test_history(self):
+        """History instance should be created on request on SearchResultView."""
+
+        from random import randint
+
+        # New Request on SearchResultView
+        current_history_count = History.objects.count()
+        self.client.get(
+            reverse('search:result', kwargs={'slug': self.result.hash_id})
+        )
+
+        self.assertEqual(
+            current_history_count + 1,
+            History.objects.count(),
+        )
+
+        # New Random Count Requests on SearchResultView
+        current_history_count = History.objects.count()
+        request_counts = randint(1, 100)
+
+        for i in range(1, request_counts + 1):
+            self.client.get(
+                reverse('search:result', kwargs={'slug': self.result.hash_id})
+            )
+            self.assertEqual(
+                current_history_count + i,
+                History.objects.count(),
+            )
--- lmstfy/search/views/result.py
+++ lmstfy/search/views/result.py
@@ -1,10 +1,19 @@
 from django.views.generic.detail import DetailView
 
 from .base import SearchBaseView
-from search.models import Result
+from search.models import Result, History
 
 
 class SearchResultView(SearchBaseView, DetailView):
     model = Result
     slug_field = 'hash_id'
     context_object_name = 'result'
+
+    def dispatch(self, *args, **kwargs):
+
+        # Create History instance on request.
+        History.objects.create(
+            result=self.get_object(),
+        )
+
+        return super(SearchResultView, self).dispatch(*args, **kwargs)
